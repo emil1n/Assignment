4049d2d6e6e95d1db4b0abef0dfbafe8584adddb
emil1n <142653565+emil1n@users.noreply.github.com>
2023-09-21 17:51:16 +0530

    index.ts

--- index.ts
+++ index.ts
@@ -6,6 +6,16 @@ let cag=new Cargo(10,"steel");
 let ast=new Astronaut(2,"metal");
 let roc=new Rocket("steel1",22)
 
-console.log(roc.addAstronaut(ast))
-console.log(roc.addCargo(cag));
+let mass = parseInt(prompt("Weight of the cargo:"));
+let name = prompt("Name of cargo:");
+const cag1 = new Cargo(mass, name);
+console.log(roc.addCargo(cag1));
+
+
+let massA = parseInt(prompt("Weight of the Astro:"));
+let nameA = prompt("Name of Astro:");
+const ast1 = new Astronaut(massA, nameA);
+console.log(roc.addAstronaut(ast1));
+//console.log(roc.addAstronaut(ast))
+//console.log(roc.addCargo(cag));
 
